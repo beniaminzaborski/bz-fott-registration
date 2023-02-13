80c03cc0d5966e88dc49e945f58264f5ec262de7
Beniamin Zaborski <beniamin.zaborski@britenet.com.pl>
2023-02-13 19:35:42 +0100

    Fix deserializaing message from Azure Service Bus in Azure Function

--- src/Bz.Fott.Registration.NumberAssignatorAzFunction/NumberAssignatorFunction.cs
+++ src/Bz.Fott.Registration.NumberAssignatorAzFunction/NumberAssignatorFunction.cs
@@ -4,6 +4,9 @@ using Npgsql;
 using System;
 using Azure.Messaging.ServiceBus;
 using Bz.Fott.Registration.Application.CompetitorRegistration;
+using Newtonsoft.Json;
+using System.Text.Json.Nodes;
+using System.Text;
 
 namespace Bz.Fott.Registration.NumberAssignatorAzFunction;
 
@@ -12,9 +15,10 @@ public class NumberAssignatorFunction
     [FunctionName("NumberAssignator")]
     public void Run([ServiceBusTrigger("registrations", Connection = "ServiceBusConnectionString")] ServiceBusReceivedMessage receivedMessage, ILogger log)
     {
-        log.LogInformation($"C# ServiceBus queue trigger function processed message: {receivedMessage.ContentType}");
-        var registerCompetitor = receivedMessage.Body.ToObjectFromJson<RegisterCompetitor>();
-        log.LogInformation($"Message after deserializing: {registerCompetitor.RequestId}, {registerCompetitor.CompetitionId}, {registerCompetitor.FirstName}");
+        // TODO: Refactor this shity code!!!
+        var jsonMsgBody = GetMessageContent(receivedMessage);
+        log.LogInformation($"C# ServiceBus queue trigger function processed message: {jsonMsgBody}");
+        var registerCompetitor = JsonConvert.DeserializeObject<RegisterCompetitor>(jsonMsgBody);
 
         var connectionString = Environment.GetEnvironmentVariable("PostgresConnectionString", EnvironmentVariableTarget.Process);
         log.LogInformation($"connectionString is {connectionString}");
@@ -35,11 +39,12 @@ public class NumberAssignatorFunction
 
             // Insert into Competitors table
             using var insertCompetitorCmd = new NpgsqlCommand(@"
-            INSERT INTO competitors (id, competitionId, number, firstName, lastName, birthDate, city, phoneNumber, contactPersonNumber) 
-            VALUES (@id, @competitionId, @number, @firstName, @lastName, @birthDate, @city, @phoneNumber, @contactPersonNumber)", conn, tran);
+            INSERT INTO competitors (""id"", ""requestId"", ""competitionId"", ""number"", ""firstName"", ""lastName"", ""birthDate"", ""city"", ""phoneNumber"", ""contactPersonNumber"") 
+            VALUES (@id, @requestId, @competitionId, @number, @firstName, @lastName, @birthDate, @city, @phoneNumber, @contactPersonNumber)", conn, tran);
             insertCompetitorCmd.Parameters.AddWithValue("id", Guid.NewGuid());
             insertCompetitorCmd.Parameters.AddWithValue("requestId", registerCompetitor.RequestId);
             insertCompetitorCmd.Parameters.AddWithValue("competitionId", registerCompetitor.CompetitionId);
+            insertCompetitorCmd.Parameters.AddWithValue("number", number);
             insertCompetitorCmd.Parameters.AddWithValue("firstName", registerCompetitor.FirstName);
             insertCompetitorCmd.Parameters.AddWithValue("lastName", registerCompetitor.LastName);
             insertCompetitorCmd.Parameters.AddWithValue("birthDate", registerCompetitor.BirthDate);
@@ -54,6 +59,12 @@ public class NumberAssignatorFunction
         {
             log.LogError($"Something went wrong for request id: {registerCompetitor.RequestId}. Details: {ex.Message}");
             tran.Rollback();
+        }
+
+        static string GetMessageContent(ServiceBusReceivedMessage receivedMessage)
+        {
+            string payload = Encoding.UTF8.GetString(receivedMessage.Body);
+            return JsonNode.Parse(payload)["message"].ToString();
         }
     }
 }
