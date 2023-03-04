47c7dcfaa5b9f03e05d6346f8e392d7e45cf6f46
Beniamin Zaborski <beniamin.zaborski@britenet.com.pl>
2023-03-04 12:54:25 +0100

    Filter list of competitors by competition id

--- src/Bz.Fott.Registration.Domain/Competitors/ICompetitorRepository.cs
+++ src/Bz.Fott.Registration.Domain/Competitors/ICompetitorRepository.cs
@@ -1,8 +1,9 @@
 ﻿using Bz.Fott.Registration.Domain.Common;
+using Bz.Fott.Registration.Domain.CompetitionIntegration;
 
 namespace Bz.Fott.Registration.Domain.Competitors;
 
 public interface ICompetitorRepository : IRepository<Competitor, CompetitorId>
 {
-
+    Task<IEnumerable<Competitor>> GetAllByCompetitionIdAsync(CompetitionId competitionId);
 }
--- src/Bz.Fott.Registration.Infrastructure/Persistence/Repositories/CompetitorRepository.cs
+++ src/Bz.Fott.Registration.Infrastructure/Persistence/Repositories/CompetitorRepository.cs
@@ -1,6 +1,8 @@
-﻿using Bz.Fott.Registration.Domain.CompetitionIntegration;
+﻿using Bz.Fott.Registration.Domain.Common;
+using Bz.Fott.Registration.Domain.CompetitionIntegration;
 using Bz.Fott.Registration.Domain.Competitors;
 using Bz.Fott.Registration.Infrastructure.Persistence.Common;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bz.Fott.Registration.Infrastructure.Persistence.Repositories;
 
@@ -8,5 +10,12 @@ internal class CompetitorRepository : Repository<Competitor, CompetitorId, Appli
 {
     public CompetitorRepository(ApplicationDbContext dbContext) : base(dbContext)
     {
+    }
+
+    public async Task<IEnumerable<Competitor>> GetAllByCompetitionIdAsync(CompetitionId competitionId)
+    {
+        return await _dbContext.Set<Competitor>()
+            .Where(c => c.CompetitionId.Equals(competitionId))
+            .ToListAsync();
     }
 }
